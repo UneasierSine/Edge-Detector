1b2262cf9ee0f6db10b60f24bce7f58026bccf8d
UneasierSine <32441247+UneasierSine@users.noreply.github.com>
2017-11-10 07:17:16 -0500

    Update Threshold.c

--- Threshold.c
+++ Threshold.c
@@ -1,1 +1,4 @@
-
+/*Do edge detection by using a light source wherein the brightness of the light is broken down to a 0 and 1 where the threshold is going to
+be changing. Lower brightness most likely indicates brightness. To prevent issues, subtract pixel brightness without lighting.*/
+#include <stdlib>
+#include <stdio>
