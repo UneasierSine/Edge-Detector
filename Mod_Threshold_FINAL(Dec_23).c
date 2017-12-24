4ab45a72c79fdeeeeb929a7ad486a404e056e828
UneasierSine <32441247+UneasierSine@users.noreply.github.com>
2017-12-23 21:54:09 -0500

    Update Mod_Threshold_FINAL(Dec_23).c

--- Mod_Threshold_FINAL(Dec_23).c
+++ Mod_Threshold_FINAL(Dec_23).c
@@ -1,77 +1,75 @@
 //December 23, 2017
+//9:49 P.M. is when I finished, and this is the most I wil do for today. Mentally exhausted? Yes. Happy? No. What? Exhilarated.
 
+//impor tlibraries
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <wiringPi.h>
 #include "imgproc.h"
 
+//camera picture dimensions
 int camWidth = 854;
 int camHeight = 480;
 int x,y;
 
+//declare functions
 int SumPixels (int r, int g, int b);
 int getBrightness(int x, int y, Image * img);
 int edgeDetector(int m, int o, int t, int th, int f);
 
 int main(int argc, char **argv)
 {
+	//initialize libraries
 	wiringPiSetup();
 	init_imgproc();
 	
+	//initialize camera and viewer
 	Camera * camera = camOpen(camWidth, camHeight);
 	Viewer * viewer = viewOpen(camWidth, camHeight, "Viewer");
+	
 	//start analysis here
 	//save image pixels that line up to array and store brightness in the array
 	while(1 == 1)
 	{
+		//take image and make a separate image for edges
 		Image * image = camGrabImage(camera);
 		Image * img = imgNew(800, 480);
+		
+		//iterate through pixels within a 480x800 range (for the display in the AR application)
 		for(x = 28; x<=827; x++)
 		{
 			for(y = 2; y<=477; y++)
 			{
+				//brightness of certain pixels in 5x5 kernel
 				int m = getBrightness(x, y, image);
-				//int dd1 = getBrightness(x-2, y-2, image);
-				//int dd2 = getBrightness(x-1, y-1, image);
-				//int dd3 = getBrightness(x+1, y+1, image);
-				//int dd4 = getBrightness(x+2, y+2, image);
 				int d1 = getBrightness(x, y-2, image);
 				int d2 = getBrightness(x, y-1, image);
 				int d3 = getBrightness(x, y+1, image);
 				int d4 = getBrightness(x, y+2, image);
-				//int ud1 = getBrightness(x-2, y+2, image);
-				//int ud2 = getBrightness(x-1, y+1, image);
-				//int ud3 = getBrightness(x+1, y-1, image);
-				//int ud4 = getBrightness(x+2, y-2, image);
 				int r1 = getBrightness(x-2, y, image);
 				int r2 = getBrightness(x-1, y, image);
 				int r3 = getBrightness(x+1, y, image);
 				int r4 = getBrightness(x+2, y, image);
-				
-			/*	if(edgeDetector(m, dd1, dd2, dd3, dd4) == 0)
-				{
-					imgSetPixel(image, x, y, 255, 0, 0);
-			*/	//}
+			
+				//edge detection and set edges to red
 				if(edgeDetector(m, d1, d2, d3, d4) == 1)
 				{
 					imgSetPixel(img, x, y, 255, 0, 0);
 				}
-	/*			else if(edgeDetector(m, ud1, ud2, ud3, ud4) == 0)
-				{
-					imgSetPixel(image, x, y, 255, 0, 0);
-				}
-		*/		if(edgeDetector(m, r1, r2, r3, r4) == 1)
+				if(edgeDetector(m, r1, r2, r3, r4) == 1)
 				{
 					imgSetPixel(img, x, y, 255, 0, 0);
 				}
 			}
 		}
+		//display image and destroy images for space
 		viewDisplayImage(viewer, img);
 		imgDestroy(image);
 		imgDestroy(img);
 	}	
 
+	//yeah that
 	viewClose(viewer);
 	camClose(camera);
 
@@ -80,11 +78,13 @@ int main(int argc, char **argv)
 	return 0;
 }
 
+//return the sum of three values
 int SumPixels (int r, int g, int b)
 {
 	return (r + g + b);
 }
 
+//get the brightness of a pixel on a scale from 0 to 765
 int getBrightness(int x, int y, Image * img)
 {
 	char * pxlVals = imgGetPixel(img, x, y);
@@ -92,6 +92,7 @@ int getBrightness(int x, int y, Image * img)
 	return brightness;
 }
 
+//edge detection algorithm
 int edgeDetector(int m, int o, int t, int th, int f)
 {
 	if(t>(m-20) && t<(m+20))
