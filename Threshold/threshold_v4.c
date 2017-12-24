0985187b911cadcd611200f5db5424f4b672f15e
UneasierSine <32441247+UneasierSine@users.noreply.github.com>
2017-12-24 17:49:34 +0000

    Create threshold_v4.c

---
+++ Threshold/threshold_v4.c
@@ -0,0 +1,90 @@
+//December 23, 2017
+/*Has changing threshold*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "imgproc.h"
+#include <math.h>
+#include <wiringPi.h>
+
+int camWidth = 960;
+int camHeight = 720;
+int x,y;
+
+int SumPixels (int r, int g, int b);
+int SmoothValue(int brightness, int val);
+
+int main(int argc, char **argv)
+{
+	wiringPiSetup();
+	init_imgproc();
+	
+	Camera * camera = camOpen(camWidth, camHeight);
+	Viewer * viewer = viewOpen(camWidth, camHeight, "Viewer");
+	//start analysis here
+	//save image pixels that line up to array and store brightness in the array
+	while(1 == 1)
+	{
+		Image * image = camGrabImage(camera);
+		for(x = 0; x < image->width; x++)
+		{
+			for(y = 0; y < image->height; y++)
+			{
+				int value = 650;
+				while(value >= 550)
+				{
+					int brightness = 0;
+					char * pixel = imgGetPixel(image, x, y);
+					char * pixelX = imgGetPixel(image, x+1, y);
+					char * pixelY= imgGetPixel(image, x, y+1);
+					brightness = SmoothValue(SumPixels(pixel[0], pixel[1], pixel[2]), value);
+					if(brightness != SmoothValue(SumPixels(pixelX[0], pixelX[1], pixelX[2]), value))
+					{
+						imgSetPixel(image, x, y, 255, 0, 0);
+						break;
+					}
+					if(brightness != SmoothValue(SumPixels(pixelY[0], pixelY[1], pixelY[2]), value))
+					{
+						imgSetPixel(image, x, y, 255, 0, 0);
+						break;
+					}
+					value = value - 50;
+				}
+			}
+		}
+		viewDisplayImage(viewer, image);
+		imgDestroy(image);
+	}	
+
+	viewClose(viewer);
+	camClose(camera);
+
+	// finally we unintialise the library
+	quit_imgproc();
+	return 0;
+}
+
+int SumPixels (int r, int g, int b)
+{
+	return (r + g + b);
+}
+
+int SmoothValue(int brightness, int val)
+{
+		if(brightness > 0)
+		{
+			if(brightness < val)
+			{
+				return 0;
+			}
+			if(brightness < 750)
+			{
+				return 255;
+			}
+			if(brightness < 765)
+			{
+				return 125;
+			}
+		}
+		return 0;
+}
